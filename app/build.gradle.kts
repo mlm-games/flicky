8c4130b1ae39de1729c357af2a5cba281fe6a8a8
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-08-27 21:15:25 +0530

    Bump maj ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 310
-        versionName = "2.3.8"
+        versionName = "2.4.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
