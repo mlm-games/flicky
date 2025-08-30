7a5160f575effe6186cd3978d065a0c50f330821
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-08-30 06:17:16 +0530

    Add few other commonly used repos (futo, brave, cryptomator, libretro)

--- app/src/main/java/app/flicky/data/model/RepositoryInfo.kt
+++ app/src/main/java/app/flicky/data/model/RepositoryInfo.kt
@@ -13,14 +13,14 @@ data class RepositoryInfo(
             RepositoryInfo("F-Droid", "https://f-droid.org/repo", true),
             RepositoryInfo("IzzyOnDroid", "https://apt.izzysoft.de/fdroid/repo", true),
             RepositoryInfo("F-Droid Archive", "https://f-droid.org/archive", false),
-            RepositoryInfo("microG", "https://microg.org/fdroid/repo", true),
+            RepositoryInfo("microG", "https://microg.org/fdroid/repo", false),
             RepositoryInfo("NewPipe upstream", "https://archive.newpipe.net/fdroid/repo", true),
 
             RepositoryInfo("Guardian Project", "https://guardianproject.info/fdroid/repo", false),
             RepositoryInfo("Briar Project", "https://briarproject.org/fdroid/repo", false),
 
             RepositoryInfo("KDE Android Release", "https://cdn.kde.org/android/stable-releases/fdroid/repo", false),
-            RepositoryInfo("KDE Android Nightly", "https://cdn.kde.org/android/nightly/fdroid/repo", false),
+            RepositoryInfo("KDE Android Nightly", "https://cdn.kde.org/android/nightly/fdroid/repo", true),
 
             RepositoryInfo("Kali NetHunter", "https://store.nethunter.com/repo", false),
 
@@ -28,7 +28,12 @@ data class RepositoryInfo(
             RepositoryInfo("Molly FOSS", "https://molly.im/fdroid/foss/fdroid/repo", false),
             RepositoryInfo("Bitwarden", "https://mobileapp.bitwarden.com/fdroid/repo", false),
             RepositoryInfo("Cromite", "https://www.cromite.org/fdroid/repo", false),
-            RepositoryInfo("IronFox", "https://fdroid.ironfoxoss.org/fdroid/repo", false)
+            RepositoryInfo("IronFox", "https://fdroid.ironfoxoss.org/fdroid/repo", false),
+            RepositoryInfo("FUTO", "https://app.futo.org/fdroid/repo", false),
+            RepositoryInfo("Brave", "https://brave-browser-apk-release.s3.brave.com/fdroid/repo", false),
+            RepositoryInfo("Cryptomator", "https://static.cryptomator.org/android/fdroid/repo", false),
+            RepositoryInfo("LibRetro", "https://fdroid.libretro.com/repo", false),
+
 
         )
     }
