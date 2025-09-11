dab58db219c5b495b55263511b8a7aaacd7444ad
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-09-11 10:04:31 +0530

    Bump major ver (might need to clear cache)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 510
-        versionName = "3.3.6"
+        versionName = "3.4.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh", "cs", "el", "fi", "ko", "nl", "vi")
