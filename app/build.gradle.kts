89893e4a8404ccadfa252fe7bf5ad4bd521ebba2
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-12-28 13:29:19 +0530

    remove unused xml files and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 720
-        versionName = "3.7.2"
+        versionName = "3.8.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh", "cs", "el", "fi", "ko", "nl", "vi")
@@ -91,10 +91,6 @@ android {
     buildFeatures {
         buildConfig = true
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "2.0.0"
     }
 
     namespace = "app.flicky"
