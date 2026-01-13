7ccdfe821d226aead8a076812b5e619181bf2ec9
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-01-13 13:38:26 +0530

    chore: Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 760
-        versionName = "3.8.4"
+        versionName = "3.9.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh", "cs", "el", "fi", "ko", "nl", "vi")
