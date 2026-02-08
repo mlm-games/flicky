eb5f9dea86916e6769557734946a01784b702d96
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-02-08 22:09:28 +0530

    chore: bump ver
    Can bump the major ver when adding animated visibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 810
-        versionName = "4.1.2"
+        versionName = "4.2.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh", "cs", "el", "fi", "ko", "nl", "vi")
